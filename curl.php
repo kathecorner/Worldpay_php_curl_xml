6892c24889c2cc570e995baa3797ce725627521e
kathecorner <bayerleverkusen672@gmail.com>
2024-05-06 15:58:04 +0900

    add index.php

---
+++ curl.php
@@ -0,0 +1,58 @@
+<?php
+$username='KVLZNJLLHCMSONK1JPSF';
+$password='DEmo2000';
+
+$xml = '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE paymentService PUBLIC "-//WorldPay//DTD WorldPay PaymentService v1//EN" "http://dtd.worldpay.com/paymentService_v1.dtd">
+<paymentService version="1.4" merchantCode="DEMO">
+ <submit>
+  <order orderCode="jsxml3688569571" installationId="1447940">
+   <description>test order</description>
+   <amount value="100" currencyCode="EUR" exponent="2"/>
+   <orderContent>
+    <![CDATA[
+     
+    ]]>
+   </orderContent>
+   <paymentMethodMask>
+    <include code="ALL"/>
+   </paymentMethodMask>
+   <shopper>
+    <shopperEmailAddress>sp@worldpay.com</shopperEmailAddress>
+   </shopper>
+   <shippingAddress>
+    <address>
+     <firstName>John</firstName>
+     <lastName>Doe</lastName>
+     <street>The Science Park</street>
+     <houseNumber>270</houseNumber>
+     <postalCode>CB4 0WE</postalCode>
+     <city>Cambridge</city>
+     <countryCode>GB</countryCode>
+    </address>
+   </shippingAddress>
+  </order>
+ </submit>
+</paymentService>';
+
+$url = "https://secure-test.worldpay.com/jsp/merchant/xml/paymentService.jsp"; // URL to make some test
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, "xml=" . $xml);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+
+$data = curl_exec($ch);
+echo '<pre>';
+echo htmlentities($data);
+echo '</pre>';
+
+if(curl_errno($ch))
+    print curl_error($ch);
+else
+    curl_close($ch);
+
+?>
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPG HPP demo</title>
+</head>
+<style>
+  * {
+    font-family: arial;
+    margin: 0px;
+  }
+
+  .purchase {
+     display:grid;
+     grid-template-columns: 100px auto;
+     grid-row-gap: 10px;
+     background-color: #D5F6F5;
+     border: 1px solid black;
+     border-radius: 10px;
+     padding: 10px;
+     font-size: 20px;
+     width: 400px;
+     margin: 30px;
+   }
+
+   .dropin {
+    margin: 20px;
+    width: 600px;
+   }
+
+   h1 {
+    padding: 0px;
+    margin: 0px;
+    margin-bottom: 10px;
+   }
+
+   input {
+     font-size: 20px;
+   }
+
+   .submit {
+     grid-column-start: 1;
+     grid-column-end: 3;
+     justify-self: center;
+   }
+
+   .submitButton {
+     background-color: rgb(13,183,73);
+     font-size: 18px;
+     border: 0px;
+     border-radius: 8px;
+     padding: 10px 15px 10px 15px;
+     margin-top: 10px;
+     cursor: pointer;
+     box-shadow: 1px 1px 2px black;
+   }
+
+    .submitButton:hover {
+     background-color: rgb(232,203,103);
+   }
+
+   #dropin-container {
+    margin:  10px;
+   }
+
+</style>
+<body>
+<form action="curl.php" method="post">
+<div class="purchase" id='checkoutDiv'>
+  <h1 class="submit">Redirect to HPP site</h1>
+   <div>Amount</div><div><input type="text" name="amount" size=10></div>
+   <div>Currency</div><div><input type="text" name="currency" size=4></div>
+   <div class="submit"><input type="submit" value="Submit"></input></div>
+</div>
+</form>
+</body>
+</html>
